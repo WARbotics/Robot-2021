9d1e325983dc9899e034d522867ce15b4caccb5f
Victor Henriksson <victor@henriksson.co>
2021-01-12 22:08:28 -0500

    WIP

--- 6925-Robot/src/main/java/frc/robot/components/Limelight.java
+++ 6925-Robot/src/main/java/frc/robot/components/Limelight.java
@@ -1,12 +1,21 @@
 package frc.robot.components;
-
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 public class Limelight{
 
-
+    NetworkTable table = NetworkTableInstance.getDefult().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+    
+    public double gettx() {
+        return (m_isPortrait) ? m_angleY : m_angleX;
+    }
     public Limelight(){
-        
+
     }
 
 }
